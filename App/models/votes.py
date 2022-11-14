57eebb6e8c30f0f54f86f5c9d84842fa74da471e
raulgoindhan <raulgoindhan@gmail.com>
2022-11-14 23:59:26 +0000

    Votes Model

---
+++ App/models/votes.py
@@ -0,0 +1,37 @@
+from App.database import db
+
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_id = db.Column(db.Integer, db.ForeignKey("review.id"), nullable=False, unique=True)
+    upvotes = db.Column(db.Integer, nullable=False)
+    downvotes = db.Column(db.Integer, nullable=False)
+    review = db.relationship(
+        "Review", backref="review", lazy=True, cascade="all, delete-orphan"
+    )
+    def __init__(self, review_id):
+        self.review_id = review_id
+        self.upvotes = 0
+        self.downvotes = 0
+    
+    def addUpVote(self, rev_id):
+        vote = Votes.query.filter_by(review_id=rev_id).first()
+        vote.upvotes = vote.upvotes + 1
+    
+    def removeUpVote(self, rev_id):
+        vote = Votes.query.filter_by(review_id=rev_id).first()
+        vote.upvotes = votes.upvotes - 1
+    
+    def addDownVote(self, rev_id):
+        vote = Votes.query.filter_by(review_id=rev_id).first()
+        vote.downvotes = vote.downvotes + 1
+    
+    def removeDownVote(self, rev_id):
+        vote = Votes.query.filter_by(review_id=rev_id).first()
+        vote.upvotes = vote.downvotes - 1
+
+    def to_json(self):
+        return {"id": self.id, 
+        "review_id": self.review_id, 
+        "upvotes": self.upvotes,
+        "downvotes": self.downvotes
+        }
