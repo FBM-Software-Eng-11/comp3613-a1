48cbde54118ab6d29003a62433be45ee4d6877e2
raulgoindhan <raulgoindhan@gmail.com>
2022-11-21 18:56:18 +0000

    forms, login, signup

--- App/views/index.py
+++ App/views/index.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory
-
+from .forms import SignUp, LogIn
 index_views = Blueprint("index_views", __name__, template_folder="../templates")
 
 
 @index_views.route("/", methods=["GET"])
 def index_page():
-    return render_template("index.html")
+    form = LogIn()
+    return render_template("login.html", form=form)
+
+@index_views.route('/signup', methods=['GET'])
+def signup():
+  form = SignUp() 
+  return render_template('signup.html', form=form) 
