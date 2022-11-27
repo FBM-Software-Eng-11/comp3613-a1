637050891c39ee8efbdc9ecd39bf07c23c9258b9
Clicked1 <cupckes422@gmail.com>
2022-11-27 19:12:19 +0000

    Wired up delete student

--- App/views/student.py
+++ App/views/student.py
@@ -2,15 +2,7 @@ from flask import Blueprint, jsonify, request, redirect, render_template, reques
 from flask_jwt import jwt_required, current_identity
 from flask_login import LoginManager, current_user, login_user, login_required, login_manager, login_required
 from App.models import *
-from App.controllers import (
-    create_student,
-    get_student,
-    get_all_students,
-    get_students_by_name,
-    get_all_student_reviews,
-    update_student,
-    delete_student,
-)
+from App.controllers import *
 
 
 student_views = Blueprint("student_views", __name__, template_folder="../templates")
@@ -60,40 +52,22 @@ def update_student_action(student_id):
     return jsonify({"error": "unauthorized"}), 401
 
 
-
-
-
-# Gets a student given student id
-@student_views.route("/api/students/<int:student_id>", methods=["GET"])
-@jwt_required()
-def get_student_action(student_id):
-    student = get_student(student_id)
-    if student:
-        return jsonify(student.to_json()), 200
-    return jsonify({"error": "student not found"}), 404
-
-
-# Gets a student given their name
-@student_views.route("/api/students/name/<string:name>", methods=["GET"])
-@jwt_required()
-def get_student_by_name_action(name):
-    students = get_students_by_name(name)
-    if students:
-        return jsonify([student.to_json() for student in students]), 200
-    return jsonify({"error": "student not found"}), 404
-
-
 # Deletes a student given student id
 # Must be an admin to access this route
-@student_views.route("/api/students/<int:student_id>", methods=["DELETE"])
-@jwt_required()
+@student_views.route("/api/students/delete/<int:student_id>")
+@login_required
 def delete_student_action(student_id):
-    if current_identity.is_admin():
+    if current_user.is_admin():
         outcome = delete_student(student_id)
+        reviews = get_reviews_by_student(student_id)
+        for review in reviews:
+            delete_review(review.id)
         if outcome:
-            return jsonify({"message": "student deleted"}), 200
-        return jsonify({"error": "student not deleted"}), 400
-    return jsonify({"error": "unauthorized"}), 401
+            flash('Student Deleted!')
+            return render_template('students.html', user = current_user, students = get_all_students())
+        flash('Student Not Deleted!')
+        return render_template('students.html', user = current_user, students = get_all_students())
+    
 
 
 # Lists all reviews for a given student.
