d8de250362e6cd05a35924905139a6c9a2cec7f0
ep1lepsy <veronramkissoon@gmail.com>
2022-11-29 16:44:41 +0000

    Added the functionality to view reviews from a single person

--- App/views/review.py
+++ App/views/review.py
@@ -14,6 +14,12 @@ def reviews_page():
     reviews = get_all_reviews()
     return render_template('reviews.html', user = current_user, students = get_all_students(), reviews=get_all_reviews())
 
+@review_views.route('/api/reviews/<int:studentId>', methods=['GET'])
+@login_required
+def getReviewsByStudent(studentId):
+    reviews = get_reviews_by_student(studentId)
+    return render_template('singleStudentReviews.html', user = current_user, student = get_student(studentId), reviews=get_reviews_by_student(studentId))
+
 # Create review given user id, student id and text
 @review_views.route("/api/reviews", methods=["POST"])
 @login_required
@@ -55,7 +61,20 @@ def vote_review_action(review_id,vote):
     flash('Error Voting!')
     return render_template('reviews.html', user = current_user, students = get_all_students(), reviews=get_all_reviews())
 
-
+# Votes on a post given post id and user id
+@review_views.route("/api/singleReviews/<int:review_id>/vote/<string:vote>/<int:studentId>", methods=["POST"])
+@login_required
+def vote_single_student_review_page(review_id,vote, studentId):
+    review = get_review(review_id)
+    if review :
+        vote = create_vote(review_id = review_id, voter_id = current_user.id, voteType = vote)
+        if vote.voteType == 'up':
+            flash("Review Upvoted!")
+        else:
+            flash("Review Downvoted!")
+        return render_template('singleStudentReviews.html', user = current_user, student = get_student(studentId), reviews=get_reviews_by_student(studentId))
+    flash('Error Voting!')
+    return render_template('singleStudentReviews.html', user = current_user, student = get_student(studentId), reviews=get_reviews_by_student(studentId))
 
 # Updates post given post id and new text
 # Only admins or the original reviewer can edit a review
